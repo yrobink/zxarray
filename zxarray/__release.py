0361da1e924c12d0a765dde38e2ad6a0fd7f3fe6
yrobin <yoann.robin.k@gmail.com>
2025-10-08 15:18:16 +0200

    add pyproject

--- zxarray/__release.py
+++ zxarray/__release.py
@@ -17,8 +17,8 @@
 ## along with zxarray.  If not, see <https://www.gnu.org/licenses/>.
 
 version_major = 0
-version_minor = 11
-version_patch = 2
+version_minor = 12
+version_patch = 0
 version_extra = ""
 version      = f"{version_major}.{version_minor}.{version_patch}{version_extra}"
 
